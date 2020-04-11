920fa69b33559ad6e1277434be74f82e2ebafa4e
GazApp <juangaz@hotmail.com>
2020-04-11 13:17:03 -0500

    1 Commit_Tim

---
+++ POO/src/Tim_Poo/Car.java
@@ -0,0 +1,72 @@
+package Tim_Poo;
+
+public class Car {
+
+	// CREATE FIELDS POR OBJECT:
+		private int doors;
+		private int wheels;
+		private String model;
+		private String engine;
+		private String colour;
+		/**
+		 * @return the doors
+		 */
+		public int getDoors() {
+			return doors;
+		}
+		/**
+		 * @param doors the doors to set
+		 */
+		public void setDoors(int doors) {
+			this.doors = doors;
+		}
+		/**
+		 * @return the wheels
+		 */
+		public int getWheels() {
+			return wheels;
+		}
+		/**
+		 * @param wheels the wheels to set
+		 */
+		public void setWheels(int wheels) {
+			this.wheels = wheels;
+		}
+		/**
+		 * @return the model
+		 */
+		public String getModel() {
+			return model;
+		}
+		/**
+		 * @param model the model to set
+		 */
+		public void setModel(String model) {
+			this.model = model;
+		}
+		/**
+		 * @return the engine
+		 */
+		public String getEngine() {
+			return engine;
+		}
+		/**
+		 * @param engine the engine to set
+		 */
+		public void setEngine(String engine) {
+			this.engine = engine;
+		}
+		/**
+		 * @return the colour
+		 */
+		public String getColour() {
+			return colour;
+		}
+		/**
+		 * @param colour the colour to set
+		 */
+		public void setColour(String colour) {
+			this.colour = colour;
+		}
+		
+}
---
+++ POO/src/Tim_Poo/Main_1.java
@@ -0,0 +1,25 @@
+package Tim_Poo;
+
+
+
+public class Main_1 {
+	
+	// Create object:
+	
+	public static void main(String[] args) {
+	Car porsche = new Car();
+	Car vento = new Car();
+	
+	porsche.setDoors(5);
+	vento.setWheels(4);
+	
+	System.out.println("This car have : "+ porsche.getDoors() + " doors");
+	
+	}
+	
+	
+	
+	
+
+
+}
